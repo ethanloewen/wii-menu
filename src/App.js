9780615fba9ce26bc219c5f23156003dade6f0f3
ethanloewen <ethanloewen@gmail.com>
2022-09-09 00:49:16 -0700

    Add MenuContainer component & add aspect ratio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import MenuContainer from './components/MenuContainer';
 
 function App() {
   return (
     <div className="App">
-    
+      <MenuContainer />
     </div>
   );
 }
---
+++ src/components/MenuContainer.js
@@ -0,0 +1,19 @@
+import './MenuContainer.scss';
+
+function MenuContainer() {
+  return (
+    <div id='menu-main'>
+        <div className='menu-container'>
+            <div className='stretchy-wrapper'>
+                <div>
+                    <div className='menu-container'>
+                            <div className='item'></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default MenuContainer;
