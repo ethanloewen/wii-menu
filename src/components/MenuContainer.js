a917348aaefbab3e83a9ed66aa98fe4ee407cefe
ethanloewen <ethanloewen@gmail.com>
2022-09-09 01:45:10 -0700

    Add rows of panels and hud

--- src/components/MenuContainer.js
+++ src/components/MenuContainer.js
@@ -2,17 +2,43 @@ import './MenuContainer.scss';
 
 function MenuContainer() {
   return (
-    <div id='menu-main'>
-        <div className='menu-container'>
+    <section id='menu-main'>
+        <div className='menu-main-container'>
             <div className='stretchy-wrapper'>
                 <div>
-                    <div className='menu-container'>
-                            <div className='item'></div>
+                    <div className='panel-container'>
+
+                        <div className='panels'>
+                            <div className='p-row'>
+                                <div className='panel'></div>
+                                <div className='panel'></div>
+                                <div className='panel'></div>
+                                <div className='panel'></div>
+                            </div>
+                            <div className='p-row'>
+                                <div className='panel'></div>
+                                <div className='panel'></div>
+                                <div className='panel'></div>
+                                <div className='panel'></div>
+                            </div>
+                            <div className='p-row'>
+                                <div className='panel'></div>
+                                <div className='panel'></div>
+                                <div className='panel'></div>
+                                <div className='panel'></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='hud-container'>
+                        <div className='hud'>
+
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
   );
 }
 
