2c1dd6f92992fbe6f118de37a2b63e68a376c3cc
ethanloewen <ethanloewen@gmail.com>
2022-10-28 21:13:46 -0700

    Change buttons to bottom on fs

--- src/components/Fullscreen.js
+++ src/components/Fullscreen.js
@@ -9,9 +9,11 @@ function Fullscreen() {
   return (
     <div id='fullscreen-main'>
       <div className='anim-panel'>
-        <div className='buttons'>
-          <button>Wii Menu</button>
-          <button>Start</button>
+        <div className='buttons-container'>
+          <div className='buttons'>
+            <button>Wii Menu</button>
+            <button>Start</button>
+          </div>
         </div>
       </div>
     </div>
