e9611dc8e04885ab45ef95fb3651fe1e0e89ed66
ethanloewen <ethanloewen@gmail.com>
2022-10-31 04:41:58 -0700

    Create reverse versions of animations

--- src/components/MenuContainer.js
+++ src/components/MenuContainer.js
@@ -6,25 +6,31 @@ import $ from "jquery";
 
 
 function MenuContainer() {
-  const [isFullscreen, setIsFullscreen] = useState(true);
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const toggleIsFullscreen = () => {
+    setIsFullscreen(current => !current);
+  };
 
   const enterFullscreen = (e) => {
     console.log(e.target.id);
     // $(`#${e.target.id}`).attr('id', 'panel-zoom');
 
     // $(`#fullscreen-main`).css('display', 'block !important');
-    setIsFullscreen(true);
+    toggleIsFullscreen();
 
     // small timeout required to make the animation work properly
     setTimeout(() => {
       $(`.anim-panel`).css('animation', `${e.target.id} 1.5s forwards`);
-      // $(`#fullscreen-main`).css('animation', 'fade-to-black 0.75s forwards');
-      $(`#fullscreen-main`).css('animation', 'fade-transition 0.5s forwards');
+      // $(`.anim-panel`).css('animation', `${e.target.id} 1.5s linear 0s 1 normal forwards`);
+      $(`#fullscreen-main`).css('animation', 'fade-dark 1.5s forwards');
+      $(`.buttons button`).css('animation', 'text-zoom 1.5s forwards');
+      $(`.panel-container`).css('animation', 'hide-panels 1.5s forwards');
       // $(`.anim-panel`).css('animation', 'panel-fullscreen-test 1.5s forwards');
       // $(`.panel-container`).css('display', 'none');
       // $(`.hud-container`).css('display', 'none');
       
-      // $(`.buttons button`).css('animation', 'text-zoom 1.5s forwards');
+      
     }, 10);
 
   };
@@ -32,11 +38,17 @@ function MenuContainer() {
   const exitFullscreen = (e) => {
     // small timeout required to make the animation work properly
     setTimeout(() => {
-      $(`.anim-panel`).css('animation', 'panel-shrink 1.5s forwards');
-      $(`.panel-container`).css('display', 'flex');
-      $(`.hud-container`).css('display', 'flex');
-      $(`.stretchy-wrapper`).css('animation', 'fade-from-black 1.5s forwards');
-      $(`.buttons button`).css('animation', 'text-zoom 1.5s reverse');
+      // $(`.panel-container`).css('visibility', `hidden`);
+      $(`.anim-panel`).css('animation', `p1-reverse 1.5s forwards`);
+      $(`#fullscreen-main`).css('animation', 'fade-light 1.5s forwards');
+      $(`.panel-container`).css('animation', 'show-panels 1.5s forwards');
+      // $(`#fullscreen-main`).css('animation', 'fade-transition 0.5s forwards');
+      // $(`.buttons button`).css('animation', 'text-zoom 1.5s forwards');
+      // $(`.anim-panel`).css('animation', 'panel-shrink 1.5s forwards');
+      // $(`.panel-container`).css('display', 'flex');
+      // $(`.hud-container`).css('display', 'flex');
+      // $(`.stretchy-wrapper`).css('animation', 'fade-from-black 1.5s forwards');
+      // $(`.buttons button`).css('animation', 'text-zoom 1.5s reverse');
     }, 10);
   };
 
@@ -46,7 +58,7 @@ function MenuContainer() {
         <div className='stretchy-wrapper'>
           <div>
 
-            <Fullscreen isFullscreen={isFullscreen} shrinkPanel={exitFullscreen} />
+            <Fullscreen isFullscreen={isFullscreen} exitFullscreen={exitFullscreen} />
 
             <div className='panel-container'>
               <div className='panels' onClick={(e) => {enterFullscreen(e)}}>
