1bd759c5346d60f079ca72996079fdd917fe4ae3
ethanloewen <ethanloewen@gmail.com>
2022-10-31 09:12:29 -0700

    Make exit animations match locations

--- src/components/MenuContainer.js
+++ src/components/MenuContainer.js
@@ -7,54 +7,39 @@ import $ from "jquery";
 
 function MenuContainer() {
   const [isFullscreen, setIsFullscreen] = useState(false);
+  const [lastId, setLastId] = useState('');
 
   const toggleIsFullscreen = () => {
     setIsFullscreen(current => !current);
   };
 
   const enterFullscreen = (e) => {
-    console.log('hit enter');
-    console.log(e.target.id);
-    // $(`#${e.target.id}`).attr('id', 'panel-zoom');
 
-    // $(`#fullscreen-main`).css('display', 'block !important');
     toggleIsFullscreen();
 
     // small timeout required to make the animation work properly
     setTimeout(() => {
       $(`.anim-panel`).css('animation', `${e.target.id} 1.5s forwards`);
-      // $(`.anim-panel`).css('animation', `${e.target.id} 1.5s linear 0s 1 normal forwards`);
       $(`#fullscreen-main`).css('animation', 'fade-dark 1.5s forwards');
       $(`.buttons button`).css('animation', 'text-grow 1.5s forwards');
       $(`.panel-container`).css('animation', 'hide-panels 1.5s forwards');
-
-
-      // $(`.anim-panel`).css('animation', 'panel-fullscreen-test 1.5s forwards');
-      // $(`.panel-container`).css('display', 'none');
-      // $(`.hud-container`).css('display', 'none');
-      
-      
     }, 10);
+
+    setLastId(`${e.target.id}`);
 
   };
 
-  const exitFullscreen = (e) => {
-    console.log('hit exit');
+  const exitFullscreen = () => {
 
     // small timeout required to make the animation work properly
     setTimeout(() => {
-      // $(`.panel-container`).css('visibility', `hidden`);
-      $(`.anim-panel`).css('animation', `p1-reverse 1.5s forwards`);
-      $(`#fullscreen-main`).css('animation', 'fade-light 1.5s forwards');
-      $(`.buttons button`).css('animation', 'text-shrink 1.5s forwards');
-      $(`.panel-container`).css('animation', 'show-panels 1.5s forwards');
-      
-      // $(`#fullscreen-main`).css('animation', 'fade-transition 0.5s forwards');
-      // $(`.anim-panel`).css('animation', 'panel-shrink 1.5s forwards');
-      // $(`.panel-container`).css('display', 'flex');
-      // $(`.hud-container`).css('display', 'flex');
-      // $(`.stretchy-wrapper`).css('animation', 'fade-from-black 1.5s forwards');
+      $(`.anim-panel`).css('animation', `${lastId}-reverse 1s forwards`);
+      $(`#fullscreen-main`).css('animation', 'fade-light 1s forwards');
+      $(`.buttons button`).css('animation', 'text-shrink 1s forwards');
+      $(`.panel-container`).css('animation', 'show-panels 1s forwards');
     }, 10);
+
+    toggleIsFullscreen();
   };
 
   return (
