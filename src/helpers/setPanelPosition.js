8166e33f5d64cd0a6decf21aecd9089dd48fb22b
ethanloewen <ethanloewen@gmail.com>
2022-10-30 21:04:50 -0700

    Add cases for each panel on menu

--- src/helpers/setPanelPosition.js
+++ src/helpers/setPanelPosition.js
@@ -1,3 +1,42 @@
-const setPanelPosition = () => {
-    
+const setPanelPosition = (id) => {
+    switch(id) {
+        case 'panel-1':
+            break;
+            
+        case 'panel-2':
+            break;
+        
+        case 'panel-3':
+            break;
+            
+        case 'panel-4':
+            break;
+
+        // case 'panel-5':
+        //     break;
+            
+        // case 'panel-6':
+        //     break;
+
+        // case 'panel-7':
+        //     break;
+            
+        // case 'panel-8':
+        //     break;
+
+        // case 'panel-9':
+        //     break;
+            
+        // case 'panel-10':
+        //     break;
+        
+        // case 'panel-11':
+        //     break;
+            
+        // case 'panel-12':
+        //     break;
+
+        default:
+            console.log('Error in setPanelPosition');
+    } 
 };
