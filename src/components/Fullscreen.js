27f4d9b070b34e15f1633c067a8a316c66ba2849
ethanloewen <ethanloewen@gmail.com>
2022-10-31 08:58:11 -0700

    Fix major visibility bug for fs

--- src/components/Fullscreen.js
+++ src/components/Fullscreen.js
@@ -6,22 +6,16 @@ import $ from "jquery";
 function Fullscreen(props) {
 
   return (
-    <>
-      {props.isFullscreen?
-        <div id='fullscreen-main'>
-          <div className='anim-panel'>
-            <div className='buttons-container'>
-              <div className='buttons'>
-                <button id='menu-button' onClick={() => {props.exitFullscreen()}}>Wii Menu</button>
-                <button id='start-button'>Start</button>
-              </div>
-            </div>
+    <div id='fullscreen-main'>
+      <div className='anim-panel'>
+        <div className='buttons-container'>
+          <div className='buttons'>
+            <button id='menu-button' onClick={() => {props.exitFullscreen()}}>Wii Menu</button>
+            <button id='start-button'>Start</button>
           </div>
         </div>
-        :
-        <></>
-      };
-    </>
+      </div>
+    </div>
   );
 }
 
--- src/components/MenuContainer.js
+++ src/components/MenuContainer.js
@@ -13,6 +13,7 @@ function MenuContainer() {
   };
 
   const enterFullscreen = (e) => {
+    console.log('hit enter');
     console.log(e.target.id);
     // $(`#${e.target.id}`).attr('id', 'panel-zoom');
 
@@ -24,8 +25,10 @@ function MenuContainer() {
       $(`.anim-panel`).css('animation', `${e.target.id} 1.5s forwards`);
       // $(`.anim-panel`).css('animation', `${e.target.id} 1.5s linear 0s 1 normal forwards`);
       $(`#fullscreen-main`).css('animation', 'fade-dark 1.5s forwards');
-      $(`.buttons button`).css('animation', 'text-zoom 1.5s forwards');
+      $(`.buttons button`).css('animation', 'text-grow 1.5s forwards');
       $(`.panel-container`).css('animation', 'hide-panels 1.5s forwards');
+
+
       // $(`.anim-panel`).css('animation', 'panel-fullscreen-test 1.5s forwards');
       // $(`.panel-container`).css('display', 'none');
       // $(`.hud-container`).css('display', 'none');
@@ -36,19 +39,21 @@ function MenuContainer() {
   };
 
   const exitFullscreen = (e) => {
+    console.log('hit exit');
+
     // small timeout required to make the animation work properly
     setTimeout(() => {
       // $(`.panel-container`).css('visibility', `hidden`);
       $(`.anim-panel`).css('animation', `p1-reverse 1.5s forwards`);
       $(`#fullscreen-main`).css('animation', 'fade-light 1.5s forwards');
+      $(`.buttons button`).css('animation', 'text-shrink 1.5s forwards');
       $(`.panel-container`).css('animation', 'show-panels 1.5s forwards');
+      
       // $(`#fullscreen-main`).css('animation', 'fade-transition 0.5s forwards');
-      // $(`.buttons button`).css('animation', 'text-zoom 1.5s forwards');
       // $(`.anim-panel`).css('animation', 'panel-shrink 1.5s forwards');
       // $(`.panel-container`).css('display', 'flex');
       // $(`.hud-container`).css('display', 'flex');
       // $(`.stretchy-wrapper`).css('animation', 'fade-from-black 1.5s forwards');
-      // $(`.buttons button`).css('animation', 'text-zoom 1.5s reverse');
     }, 10);
   };
 
