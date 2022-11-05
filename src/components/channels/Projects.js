d819e01623357254e33de1cabbb43447a99fab0f
ethanloewen <ethanloewen@gmail.com>
2022-11-05 03:27:10 -0700

    Make spinner for channel anim

--- src/components/channels/Projects.js
+++ src/components/channels/Projects.js
@@ -7,7 +7,30 @@ function Projects(props) {
 
   return (
     <div id='projects-main'>
-        
+        <div className='spinner s1'>
+            <div className='spin-item'></div>
+        </div>
+        <div className='spinner s2'>
+            <div className='spin-item'></div>
+        </div>
+        <div className='spinner s3'>
+            <div className='spin-item'></div>
+        </div>
+        <div className='spinner s4'>
+            <div className='spin-item'></div>
+        </div>
+        <div className='spinner s5'>
+            <div className='spin-item'></div>
+        </div>
+        <div className='spinner s6'>
+            <div className='spin-item'></div>
+        </div>
+        <div className='spinner s7'>
+            <div className='spin-item'></div>
+        </div>
+        <div className='spinner s8'>
+            <div className='spin-item'></div>
+        </div>
     </div>
   );
 }
