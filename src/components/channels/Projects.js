5150653b55b022697f01bf0057073de3bc81f3bf
ethanloewen <ethanloewen@gmail.com>
2022-11-06 01:46:08 -0800

    Re-add hidden spinners & restyle

--- src/components/channels/Projects.js
+++ src/components/channels/Projects.js
@@ -5,55 +5,57 @@ import $ from "jquery";
 
 function Projects(props) {
 
-  return (
-    <div id='projects-main'>
+    return (
+        <div id='projects-main'>
 
-        <div className='project-title'>
-            <h1>:Tiny URL</h1>
-        </div>
-
-        <div className='spinner-container'>
-            <div className='spinner s1'>
-                <div className='spin-item'></div>
+            <div className='project-title'>
+                <h1>:Tiny URL</h1>
             </div>
-        </div>
 
-        
-        {/* <div className='spinner s2'>
-            <div className='spin-item'></div>
+            <div className='spinner-container'>
+                <div className='spinner s1'>
+                    <div className='spin-item'></div>
+                </div>
+
+                <div className='spinner s2'>
+                    <div className='spin-item'></div>
+                </div>
+                <div className='spinner s3'>
+                    <div className='spin-item'></div>
+                </div>
+                <div className='spinner s4'>
+                    <div className='spin-item'></div>
+                </div>
+                <div className='spinner s5'>
+                    <div className='spin-item'></div>
+                </div>
+                <div className='spinner s6'>
+                    <div className='spin-item'></div>
+                </div>
+                <div className='spinner s7'>
+                    <div className='spin-item'></div>
+                </div>
+                <div className='spinner s8'>
+                    <div className='spin-item'></div>
+                </div>
+                <div className='spinner s9'>
+                    <div className='spin-item'></div>
+                </div>
+                <div className='spinner s10'>
+                    <div className='spin-item'></div>
+                </div>
+                <div className='spinner s11'>
+                    <div className='spin-item'></div>
+                </div>
+                <div className='spinner s12'>
+                    <div className='spin-item'></div>
+                </div>
+            </div>
+
+
+
         </div>
-        <div className='spinner s3'>
-            <div className='spin-item'></div>
-        </div>
-        <div className='spinner s4'>
-            <div className='spin-item'></div>
-        </div>
-        <div className='spinner s5'>
-            <div className='spin-item'></div>
-        </div>
-        <div className='spinner s6'>
-            <div className='spin-item'></div>
-        </div>
-        <div className='spinner s7'>
-            <div className='spin-item'></div>
-        </div>
-        <div className='spinner s8'>
-            <div className='spin-item'></div>
-        </div>
-        <div className='spinner s9'>
-            <div className='spin-item'></div>
-        </div>
-        <div className='spinner s10'>
-            <div className='spin-item'></div>
-        </div>
-        <div className='spinner s11'>
-            <div className='spin-item'></div>
-        </div>
-        <div className='spinner s12'>
-            <div className='spin-item'></div>
-        </div> */}
-    </div>
-  );
+    );
 }
 
 export default Projects;
