828186696ef5c94044e8bbcc39c374875d18daee
ethanloewen <ethanloewen@gmail.com>
2022-11-08 21:10:07 -0800

    Add and place icons

--- src/components/channels/Projects.js
+++ src/components/channels/Projects.js
@@ -1,12 +1,19 @@
 import './Projects.scss';
-import $ from "jquery";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFolderOpen } from '@fortawesome/free-regular-svg-icons';
+import { faFolder } from '@fortawesome/free-solid-svg-icons';
+
 
 
 
 function Projects(props) {
 
+    const iconColor = '#1a1a1a';
+    const iconSize = '4x';
+
     return (
         <div id='projects-main'>
+
 
             <div className='project-title'>
                 <h1>:Tiny URL</h1>
@@ -15,41 +22,65 @@ function Projects(props) {
             <div className='spinner-container'>
 
                 <div className='spinner s1'>
-                    <div className='spin-item'></div>
+                    <div className='spin-item'>
+                        <FontAwesomeIcon icon={faFolderOpen} size={iconSize} color={iconColor} />
+                    </div>
                 </div>
 
                 <div className='spinner s2'>
-                    <div className='spin-item'></div>
+                    <div className='spin-item'>
+                        <FontAwesomeIcon icon={faFolder} size={iconSize} />
+                    </div>
                 </div>
                 <div className='spinner s3'>
-                    <div className='spin-item'></div>
+                    <div className='spin-item'>
+                        <FontAwesomeIcon icon={faFolder} size={iconSize} />
+                    </div>
                 </div>
                 <div className='spinner s4'>
-                    <div className='spin-item'></div>
+                    <div className='spin-item'>
+                        <FontAwesomeIcon icon={faFolder} size={iconSize} />
+                    </div>
                 </div>
                 <div className='spinner s5'>
-                    <div className='spin-item'></div>
+                    <div className='spin-item'>
+                        <FontAwesomeIcon icon={faFolder} size={iconSize} />
+                    </div>
                 </div>
                 <div className='spinner s6'>
-                    <div className='spin-item'></div>
+                    <div className='spin-item'>
+                        <FontAwesomeIcon icon={faFolder} size={iconSize} />
+                    </div>
                 </div>
                 <div className='spinner s7'>
-                    <div className='spin-item'></div>
+                    <div className='spin-item'>
+                        <FontAwesomeIcon icon={faFolder} size={iconSize} />
+                    </div>
                 </div>
                 <div className='spinner s8'>
-                    <div className='spin-item'></div>
+                    <div className='spin-item'>
+                        <FontAwesomeIcon icon={faFolder} size={iconSize} />
+                    </div>
                 </div>
                 <div className='spinner s9'>
-                    <div className='spin-item'></div>
+                    <div className='spin-item'>
+                        <FontAwesomeIcon icon={faFolder} size={iconSize} />
+                    </div>
                 </div>
                 <div className='spinner s10'>
-                    <div className='spin-item'></div>
+                    <div className='spin-item'>
+                        <FontAwesomeIcon icon={faFolder} size={iconSize} />
+                    </div>
                 </div>
                 <div className='spinner s11'>
-                    <div className='spin-item'></div>
+                    <div className='spin-item'>
+                        <FontAwesomeIcon icon={faFolder} size={iconSize} />
+                    </div>
                 </div>
                 <div className='spinner s12'>
-                    <div className='spin-item'></div>
+                    <div className='spin-item'>
+                        <FontAwesomeIcon icon={faFolder} size={iconSize} />
+                    </div>
                 </div>
                 
             </div>
