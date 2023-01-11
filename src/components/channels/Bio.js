7611703e9cd036199c356b5a10be8533ebf3791a
Ethan Loewen <ethanloewen@gmail.com>
2023-01-10 23:20:22 -0800

    Style bio page

--- src/components/channels/Bio.js
+++ src/components/channels/Bio.js
@@ -7,7 +7,18 @@ function Bio(props) {
     return (
         <div id='bio-main'>
             <div className='img-wrap'>
-                <img src='/ethan_mii.png' alt='ethan-mii' />
+                <img src='/ethan_mii_v2.png' alt='ethan-mii' />
+                <div className='circ'></div>
+                <div className='circ'></div>
+                <div className='circ'></div>
+            </div>
+            <div className='bio-text'>
+                <h1>Hi, I'm Ethan</h1>
+                <h2>A fullstack web developer from Canada</h2>
+                {/* <div className='wrapper'>
+                    <img src='/wii_button_v2.png' alt='ethan-mii' />
+                    <div className='circ'></div>
+                </div> */}
             </div>
         </div>
     );
