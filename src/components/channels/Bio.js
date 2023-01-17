18d851f4cb7c9d39f079a0dabdac66db8593fe3f
Ethan Loewen <ethanloewen@gmail.com>
2023-01-17 10:42:45 -0800

    Style bio links for extra info

--- src/components/channels/Bio.js
+++ src/components/channels/Bio.js
@@ -17,6 +17,29 @@ function Bio(props) {
             <div className='bio-text'>
                 <h1>Hi, I'm Ethan</h1>
                 <h2>A fullstack web developer from Canada</h2>
+                <div className='links-section' id='external-links'>
+                    <div className='icon'>
+                        <i class="fa-brands fa-github fa-5x"></i>
+                    </div>
+                    <div className='icon resume'>
+                        <i class="fa-solid fa-file-code fa-5x">
+                            
+                        </i>
+                        {/* <div className='resume-circle'>
+                            
+                        </div> */}
+                    </div>
+                    <div className='icon'>
+                        <i class="fa-brands fa-linkedin fa-5x"></i>
+                    </div>
+                </div>
+                <div className='links-section' id='link-details'>
+                    <img className='drawn-circ' src='/drawn_circ.png' alt='hand-drawn-circle' />
+                    <img className='drawn-arrow' src='/drawn_arrow.png' alt='hand-drawn-arrow' />
+                    <div className='resume-text'>
+                        <p>get my resume</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
