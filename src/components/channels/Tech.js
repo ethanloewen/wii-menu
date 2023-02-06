0960ec5b14ec23738a2af20650a7d083c6100c08
Ethan Loewen <ethanloewen@gmail.com>
2023-02-06 13:01:06 -0800

    Add titles to each card

--- src/components/channels/Tech.js
+++ src/components/channels/Tech.js
@@ -15,6 +15,7 @@ function Tech(props) {
 
             <div className='cards-container'>
                 <div className='card'>
+                
                     <div className='suit-wrapper'>
                         <h2>A</h2>
                         <FontAwesomeIcon className='icon' icon={faLanguage} />
@@ -24,6 +25,8 @@ function Tech(props) {
                         <h2>A</h2>
                         <FontAwesomeIcon className='icon' icon={faLanguage} />
                     </div>
+
+                    <h3>Languages</h3>
                 </div>
 
                 <div className='card'>
@@ -36,6 +39,8 @@ function Tech(props) {
                         <h2>K</h2>
                         <FontAwesomeIcon className='icon' icon={faCubes} />
                     </div>
+
+                    <h3>Frameworks</h3>
                 </div>
 
                 <div className='card'>
@@ -48,6 +53,8 @@ function Tech(props) {
                         <h2>Q</h2>
                         <FontAwesomeIcon className='icon' icon={faDatabase} />
                     </div>
+
+                    <h3>Databases</h3>
                 </div>
 
                 <div className='card'>
@@ -60,6 +67,8 @@ function Tech(props) {
                         <h2>J</h2>
                         <FontAwesomeIcon className='icon' icon={faPenRuler} />
                     </div>
+
+                    <h3>Design</h3>
                 </div>
             </div>
         </div>
