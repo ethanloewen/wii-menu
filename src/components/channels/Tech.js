66253cd9f8166b74c20b992a9ffc996d1d729e91
Ethan Loewen <ethanloewen@gmail.com>
2023-02-07 09:32:46 -0800

    Add remaining icons & grids

--- src/components/channels/Tech.js
+++ src/components/channels/Tech.js
@@ -26,7 +26,7 @@ function Tech(props) {
                         <FontAwesomeIcon className='icon' icon={faLanguage} />
                     </div>
 
-                    <div className='card-body'>
+                    <div className='card-body c1'>
                         <div className='icon-group'>
                             <i class="devicon-html5-plain"></i>
                             <p>HTML</p>
@@ -56,7 +56,6 @@ function Tech(props) {
                             <img src='/sql-server.png' />
                             <p>SQL</p>
                         </div>
-
                     </div>
 
                     <h3>Languages</h3>
@@ -73,6 +72,48 @@ function Tech(props) {
                         <FontAwesomeIcon className='icon' icon={faCubes} />
                     </div>
 
+                    <div className='card-body c2'>
+                        <div className='icon-group'>
+                            <i class="devicon-nodejs-plain"></i>
+                            <p>NodeJS</p>
+                        </div>
+
+                        <div className='icon-group'>
+                            <i class="devicon-react-original"></i>
+                            <p>React</p>
+                        </div>
+
+                        <div className='icon-group'>
+                            <i class="devicon-sass-original"></i>
+                            <p>SASS</p>
+                        </div>
+
+                        <div className='icon-group'>
+                            <i class="devicon-express-original"></i>
+                            <p>Express</p>
+                        </div>
+
+                        <div className='icon-group'>
+                            <i class="devicon-rails-plain"></i>
+                            <p>Rails</p>
+                        </div>
+
+                        <div className='icon-group'>
+                            <i class="devicon-jquery-plain"></i>
+                            <p>jQuery</p>
+                        </div>
+
+                        <div className='icon-group'>
+                            <i class="devicon-django-plain"></i>
+                            <p>Django</p>
+                        </div>
+
+                        <div className='icon-group'>
+                            <i class="devicon-threejs-original"></i>
+                            <p>Three.js</p>
+                        </div>
+                    </div>
+
                     <h3>Frameworks</h3>
                 </div>
 
@@ -87,6 +128,18 @@ function Tech(props) {
                         <FontAwesomeIcon className='icon' icon={faDatabase} />
                     </div>
 
+                    <div className='card-body c3'>
+                        <div className='icon-group'>
+                            <i class="devicon-postgresql-plain"></i>
+                            <p>PostgreSQL</p>
+                        </div>
+
+                        <div className='icon-group'>
+                            <i class="devicon-mysql-plain"></i>
+                            <p>MySQL</p>
+                        </div>
+                    </div>
+
                     <h3>Databases</h3>
                 </div>
 
@@ -99,6 +152,28 @@ function Tech(props) {
                     <div className='suit-wrapper'>
                         <h2>J</h2>
                         <FontAwesomeIcon className='icon' icon={faPenRuler} />
+                    </div>
+
+                    <div className='card-body c4'>
+                        <div className='icon-group'>
+                            <i class="devicon-photoshop-plain"></i>
+                            <p>Photoshop</p>
+                        </div>
+
+                        <div className='icon-group'>
+                            <i class="devicon-figma-plain"></i>
+                            <p>Figma</p>
+                        </div>
+
+                        <div className='icon-group'>
+                            <i class="devicon-blender-original"></i>
+                            <p>Blender</p>
+                        </div>
+
+                        <div className='icon-group'>
+                            <i class="devicon-unity-original"></i>
+                            <p>Unity</p>
+                        </div>
                     </div>
 
                     <h3>Design</h3>
